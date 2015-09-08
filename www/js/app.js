c521bb74a899a201a730797ebda6f36fb5571fb6
Wayne Hutchinson <wfh2727@gmail.com>
2015-09-08 09:36:50 -0500

    initial commit with facebook auth

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,7 +30,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
-  })
+  })    
 
   .state('app.search', {
     url: '/search',
@@ -39,7 +39,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         templateUrl: 'templates/search.html'
       }
     }
-  })
+  })  
 
   .state('app.browse', {
       url: '/browse',
@@ -67,7 +67,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
-  });
+  }); 
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
