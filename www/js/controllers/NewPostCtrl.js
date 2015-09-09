262ecba484acf235f4c068bf2bde3cd5317c379b
Sally Fish(er) <sqfisher@gmail.com>
2015-09-09 05:41:36 -0500

    Add post form fully functioning. Uploads user photo from photo library to imgur and post form data along with photo url to firebase

--- www/js/controllers/NewPostCtrl.js
+++ www/js/controllers/NewPostCtrl.js
@@ -1,6 +1,6 @@
-angular.module('starter.newpostform', ["newpoststorage"])
-.controller('NewPostCtrl', ["$scope", "$firebaseArray", "$location", "newpost",
-  function($scope, $firebaseArray, $location, storage) {
+angular.module('starter.newpostform', ["newpoststorage", "ngCordova"])
+.controller('NewPostCtrl', ["$scope", "$firebaseArray", "$location", "newpost", "$cordovaCamera", "$cordovaFileTransfer",
+  function($scope, $firebaseArray, $location, storage, $cordovaCamera, $cordovaFileTransfer) {
     var ref = new Firebase("https://neighbor-board.firebaseio.com/posts");
     $scope.postlist = $firebaseArray(ref);
     $scope.categoryObject = "";
@@ -18,8 +18,8 @@ angular.module('starter.newpostform', ["newpoststorage"])
         "zip": ""
       },
       "timestamp": {
-        "date": "",
-        "time": ""
+        "eventDate": "",
+        "eventTime": ""
       },
       "uid": ""
     };
@@ -27,11 +27,52 @@ angular.module('starter.newpostform', ["newpoststorage"])
       $scope.categoryObject = storage.get("category");
       $scope.newPost.category = $scope.categoryObject.title;
     });
-    $scope.postData = function() {
-      var newDate = document.getElementById("newDate").value;
-      $scope.newPost.timestamp.date = document.getElementById("newDate").value;
-      $scope.newPost.timestamp.time = document.getElementById("newTime").value;
-      $scope.postlist.$add($scope.newPost);
-    }
-  }
+
+    document.addEventListener("deviceready", function () {
+      $scope.openPhotoLibrary = function() {
+          var options = {
+              quality: 50,
+              destinationType: Camera.DestinationType.FILE_URI,
+              sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+              allowEdit: true,
+              encodingType: Camera.EncodingType.JPEG,
+              popoverOptions: CameraPopoverOptions,
+              saveToPhotoAlbum: false
+          };
+          $cordovaCamera.getPicture(options).then(function(imageData) {
+              $scope.newPost.content.image = imageData;  
+              var image = document.getElementById('tempImage');
+              image.src = imageData;
+
+              var imageData = $scope.newPost.content.image;
+              var server = "https://api.imgur.com/3/image.json",
+                  filePath = imageData,
+                  headers = {'Authorization': 'Client-ID 4ff4d561a18ac75'};
+              var options = {
+                headers: headers,
+                fileKey: "image",
+                fileName: imageData.substr(imageData.lastIndexOf('/') + 1),
+                mimeType: "image/jpg"
+              };
+              $cordovaFileTransfer.upload(server, filePath, options).then(function(result) {
+                var responseData = JSON.parse(result.response);
+                $scope.newPost.content.image = responseData.data.link;
+              }, function(err) {
+                  console.log("ERROR: " + JSON.stringify(err));
+              }, function (progress) {
+                  // progress updates
+              });
+          }, function(err) {
+              console.log(err);
+          });
+      };
+      $scope.postData = function() {
+        $scope.newPost.timestamp.post = (new Date()).toLocaleString();
+        $scope.newPost.timestamp.eventDate = document.getElementById("newDate").value;
+        $scope.newPost.timestamp.eventTime = document.getElementById("newTime").value;
+        $scope.postlist.$add($scope.newPost);
+        $location.url('/');
+      };
+    }, false);
+}
 ]);
---
+++ www/js/services.js
@@ -0,0 +1,135 @@
+angular.module('starter')
+ 
+.factory('FileService', function() {
+  var images;
+  var IMAGE_STORAGE_KEY = 'images';
+ 
+  function getImages() {
+    var img = window.localStorage.getItem(IMAGE_STORAGE_KEY);
+    if (img) {
+      images = JSON.parse(img);
+    } else {
+      images = [];
+    }
+    return images;
+  };
+ 
+  function addImage(img) {
+    images.push(img);
+    window.localStorage.setItem(IMAGE_STORAGE_KEY, JSON.stringify(images));
+  };
+ 
+  return {
+    storeImage: addImage,
+    images: getImages
+  }
+})
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+.factory('ImageService', function($cordovaCamera, FileService, $q, $cordovaFile) {
+ 
+  function makeid() {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+ 
+    for (var i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+ 
+  function optionsForType(type) {
+    var source;
+    switch (type) {
+      case 0:
+        source = Camera.PictureSourceType.CAMERA;
+        break;
+      case 1:
+        source = Camera.PictureSourceType.PHOTOLIBRARY;
+        break;
+    }
+    return {
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: source,
+      allowEdit: false,
+      encodingType: Camera.EncodingType.JPEG,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+  }
+
+  function saveMedia(type) {
+    return $q(function(resolve, reject) {
+      var options = optionsForType(type);
+ 
+      $cordovaCamera.getPicture(options).then(function(imageUrl) {
+        var name = imageUrl.substr(imageUrl.lastIndexOf('/') + 1);
+        var namePath = imageUrl.substr(0, imageUrl.lastIndexOf('/') + 1);
+        var newName = makeid() + name;
+        $cordovaFile.copyFile(namePath, name, cordova.file.dataDirectory, newName)
+          .then(function(info) {
+            FileService.storeImage(newName);
+            resolve();
+          }, function(e) {
+            reject();
+          });
+      });
+    })
+  }
+  return {
+    handleMediaDialog: saveMedia
+  }
+});
